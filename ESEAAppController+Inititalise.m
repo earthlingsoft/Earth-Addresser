513e697e8bafca94eb7a7b0827e7e49c31733903
Sven-S. Porst <ssp-web@earthlingsoft.net>
2014-07-29 01:32:08 +0200

    Move +initialize (with standard defaults) to its own file

---
+++ ESEAAppController+Inititalise.m
@@ -0,0 +1,71 @@
+//
+//  ESEAAppController+StandardDefaults.m
+//  Earth Addresser
+//
+//  Created by Sven on 29.07.14.
+//  Copyright (c) 2014 earthlingsoft. All rights reserved.
+//
+
+#import "ESEAAppController.h"
+#import "ESTerm.h"
+
+@implementation ESEAAppController (Initialise)
+
++ (void) initialize {
+	NSDictionary * standardDefaults = @{
+		@"dontShowWarning": @NO,
+		@"imageSize": @1.5,
+		@"addressBookScope": @0,
+		@"placemarkWithName": @YES,
+		@"placemarkWithLabel": @YES,
+		@"placemarkNameReplacement": @"\342\235\200",
+		@"placemarkWithAddress": @YES,
+		@"placemarkWithImage": @YES,
+		@"placemarkWithEMail": @NO,
+		@"placemarkWithPhone": @NO,
+		@"placemarkWithWeblinks": @YES,
+		@"placemarkWithAddressBookLink": @YES,
+		@"placemarkWithContacts": @NO,
+		@"placemarkWithNotes": @NO,
+		@"noHomeWorkIcons": @NO,
+		@"hasReadInfo": @NO,
+		@"groupByAddressLabel": @NO,
+		@"secondsBetweenCoordinateLookups": @1.0,
+		@"addressTermsToRemove": @[
+				@{ESTermStringKey:@"c/o ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Geb. ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@" Dept", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Dept ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Dept.", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Department ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@" Department", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Zi. ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Zimmer ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Room ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Raum ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"University of", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Universität ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Flat ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"App ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"App.", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Apt ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Apt.", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"#", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"P.O. Box", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"P.O.Box", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Postfach ", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Büro", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Office", ESTermActiveKey:@YES}
+				],
+		@"oldLabels": @[
+				@{ESTermStringKey:@"Old", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Alt", ESTermActiveKey:@YES},
+				@{ESTermStringKey:@"Ancienne", ESTermActiveKey:@YES},
+				]
+		};
+	
+	[UDC setInitialValues:standardDefaults];
+	[UDC setAppliesImmediately:YES];
+}
+
+@end
